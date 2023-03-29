93c8556087742eeaec0678cc481031800ae34ae9
Lal0g <edujosgonzalezc@gmail.com>
2023-03-29 01:35:50 -0600

    fixing bugs

--- EDD_Proyecto1_Fase2/Code/Login/js/mainproject.js
+++ EDD_Proyecto1_Fase2/Code/Login/js/mainproject.js
@@ -1,1 +1,12 @@
 
+function Loginn() {
+   
+    var user = document.getElementById("useeer").value;
+			var pass = document.getElementById("passsword").value;
+			if(user == "admin" && pass == "admin"){
+				location.href = "../Dashboard/examples/dashboard.html";
+				alert("Bienvenido");
+			}else{
+				alert("Usuario o contraseña incorrectos");
+			}
+}
